82a73b04b3371519fb274d725c1b0f6771d3a860
passkwall <apcipollone3@gmail.com>
2018-04-22 12:10:03 -0700

    this is a test

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',  log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append the message');
+    };
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Woah there!',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    loginGreeting: 'Welcome to your website',
+    currentYear: new Date().getFullYear()
+  });
+  // res.send('<h1>Hello express!</h1>');
+  // res.send({
+  //   name: 'Pasquale',
+  //   likes: [
+  //     'Skateboards',
+  //     'Oakland',
+  //     'Stupid code'
+  //   ],
+  // });
+
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: '400',
+    type: 'invalid request',
+    errorMessage: 'Could not find what you are looking for'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
